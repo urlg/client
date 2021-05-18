9665a66d8f78842f30ae1c03531ba0b62f533d08
1400121424 <14001214242QQ.COM>
2021-05-18 23:37:22 +0800

    提交流程跑通的版本

--- src/main/java/com/demo/AcbpApplication.java
+++ src/main/java/com/demo/AcbpApplication.java
@@ -1,9 +1,11 @@
 package com.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.demo")
 public class AcbpApplication {
 
     public static void main(String[] args) {
