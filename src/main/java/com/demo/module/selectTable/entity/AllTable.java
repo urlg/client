26902b2863e571b7b25af885b2ba0b6d3de74e92
1400121424 <14001214242QQ.COM>
2021-05-22 00:03:19 +0800

    新增查询MYSQL数据的所有表

--- src/main/java/com/demo/module/selectTable/entity/AllTable.java
+++ src/main/java/com/demo/module/selectTable/entity/AllTable.java
@@ -1,6 +1,38 @@
 package com.demo.module.selectTable.entity;
 
+import java.sql.Date;
+import java.util.Objects;
+
 public class AllTable {
+    String tableName;
+    String tableType;
+    int tableRows;
+    Date createTime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AllTable allTable = (AllTable) o;
+        return tableRows == allTable.tableRows &&
+                tableName.equals(allTable.tableName) &&
+                tableType.equals(allTable.tableType) &&
+                createTime.equals(allTable.createTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tableName, tableType, tableRows, createTime);
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
     public String getTableName() {
         return tableName;
     }
@@ -9,5 +41,20 @@ public class AllTable {
         this.tableName = tableName;
     }
 
-    String tableName;
+    public String getTableType() {
+        return tableType;
+    }
+
+    public void setTableType(String tableType) {
+        this.tableType = tableType;
+    }
+
+    public int getTableRows() {
+        return tableRows;
+    }
+
+    public void setTableRows(int tableRows) {
+        this.tableRows = tableRows;
+    }
+
 }
